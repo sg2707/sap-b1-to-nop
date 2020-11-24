11e558463f121943067a8dbb33355bacd4270c06
saif <saiful.haque@appfinity.co.in>
2020-11-24 10:58:59 +0530

    Updated Product Sync

--- SyncToNopCommerce/Scripts/SI_NopCommerceProduct.sql
+++ SyncToNopCommerce/Scripts/SI_NopCommerceProduct.sql
@@ -3,12 +3,25 @@
  as
 
  begin
-select Product.ItemCode [sku], U_SI_PartNumber [manufacturer_part_number],CodeBars [gtin]
+select  Product.ItemCode [sku], U_SI_PartNumber [manufacturer_part_number],CodeBars [gtin]
 ,concat (U_SI_Brand,' ',Grp.U_SI_ItemClassDesc,U_SI_PartNumber) [name],
-concat (U_SI_Brand,Grp.ItemClass) [short_description],isnull(U_SI_Length,0) [length]
-,isnull(U_SI_Width,0) [width],isnull(U_SI_Height,0) [height],isnull(U_SI_Weight,0) [weight],
-concat (U_SI_Brand,Grp.ItemClass) [full_description] ,U_SI_Brand [manufacturer],
-Grp.U_SI_MainGroup  [category]
+concat (U_SI_Brand,Grp.ItemClass) [short_description],
+concat (U_SI_Brand,Grp.ItemClass,
+	(select distinct stuff((select ','+ T2.U_Value from [OITM] T0
+  join [@SI_ITEMGROUP_ATTR] T1 on T0.ItmsGrpCod = T1.U_EntityClassID
+  join [@SI_ITEM_ATTRIBUTE] T2 on T1.U_AttributeID = T2.U_AttributeID
+  join [@SI_ATTRIBUTE] T3 on T2.U_AttributeID = T3.U_AttributeID where t0.ItemCode=Product.ItemCode and T1.U_Printable =1
+
+ for xml path('')
+    ),1,1,'') as userlist
+from  [OITM] T0
+  join [@SI_ITEMGROUP_ATTR] T1 on T0.ItmsGrpCod = T1.U_EntityClassID
+  join [@SI_ITEM_ATTRIBUTE] T2 on T1.U_AttributeID = T2.U_AttributeID
+  join [@SI_ATTRIBUTE] T3 on T2.U_AttributeID = T3.U_AttributeID where t0.ItemCode=Product.ItemCode and T1.U_Printable =1
+group by T2.U_Value )
+) [full_description] ,U_SI_Brand [manufacturer],
+Grp.U_SI_MainGroup  [category],isnull(U_SI_Length,0) [length]
+,isnull(U_SI_Width,0) [width],isnull(U_SI_Height,0) [height],isnull(U_SI_Weight,0) [weight]
 , 10.00 [price], cast((Stock.OnHand - Stock.IsCommited)as int) as stock_quantity
 from OITM [Product]
 inner join [OITW] Stock on Product.ItemCode = Stock.ItemCode and Stock.WhsCode = '01'
@@ -18,6 +31,7 @@ where frozenFor = 'N' and  Grp.U_SI_MainGroup is not null and ( (cast(concat(Con
 > @LastProductSync or (cast(concat(Convert(date,Product.UpdateDate),
 ' ', convert(char(8), dateadd(second, Product.UpdateTS, '') , 114)) as datetime))
 > @LastProductSync) 
+
 
 end
 
