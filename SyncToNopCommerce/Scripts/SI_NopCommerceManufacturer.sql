cdfee602e837acd32bc9dad47ffbacc5d5281275
saif <saiful.haque@appfinity.co.in>
2020-12-10 16:03:24 +0530

    Updated vehicle sync

--- SyncToNopCommerce/Scripts/SI_NopCommerceManufacturer.sql
+++ SyncToNopCommerce/Scripts/SI_NopCommerceManufacturer.sql
@@ -1,14 +1,13 @@
 create procedure SI_NopCommerceManufacturer @LastManufacturSync datetime
  as
  begin
-select U_SI_Brand [name],max((cast(concat(Convert(date,CreateDate),
-' ', convert(char(8), dateadd(second, createts, '') , 114)) as datetime))) [CreatedDate],
- max((cast(concat(Convert(date,UpdateDate),
-' ', convert(char(8), dateadd(second,UpdateTS, '') , 114)) as datetime))) [UpdatedDate]
+select U_SI_Brand [name]
 from OITM group by U_SI_Brand having(
-max((cast(concat(Convert(date,CreateDate),
-' ', convert(char(8), dateadd(second, createts, '') , 114)) as datetime)))
-> @LastManufacturSync or max((cast(concat(Convert(date,UpdateDate),
-' ', convert(char(8), dateadd(second,UpdateTS, '') , 114)) as datetime)))
-> @LastManufacturSync) and U_SI_Brand is not null
+ U_SI_Brand is not null and(
+ (max(CreateDate) > cast(@LastManufacturSync as DATE) or 
+ (max(CreateDate) = cast(@LastManufacturSync as DATE) AND max(createts) >=  FORMAT(@LastManufacturSync,'HHmmss')))  or
+ (max(UpdateDate)> cast(@LastManufacturSync as DATE) or 
+ (max(UpdateDate) = cast(@LastManufacturSync as DATE) AND max(UpdateTS) >=  FORMAT(@LastManufacturSync,'HHmmss')))) )
 end
+
+
